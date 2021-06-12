3863267e40c5efe7bf43a0a3bc7d973c98355a58
Petrov Kirill <ottokirik@gmail.com>
2021-06-12 12:52:19 +0300

    Init: инициализация проекта

---
+++ utils-server/connect-db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectToDatabse = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('Mongodb connected.');
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+
+  return client;
+};
+
+module.exports = connectToDatabse;
